d1e1fa460630a21702f0b067c694a79fd562071a
SungWon Kwak <one_0225@yahoo.com>
2013-03-29 02:33:06 -0700

    Update pca4_spec_query.py

--- pca4_spec_query.py
+++ pca4_spec_query.py
@@ -5,19 +5,21 @@ from astroML.datasets import fetch_sdss_corrected_spectra
 from astroML.datasets import fetch_sdss_spectrum
 from astroML.datasets.tools import query_plate_mjd_fiber, TARGET_GALAXY_RED
 
-primtarget=TARGET_GALAXY_RED
+#-----limit z-----#
+#http://www.sdss.org/dr2/products/general/edr_html/node53.html
 zlim=(0.42, 0.55)
+primtarget=TARGET_GALAXY_RED
 
+#-----getting list of plate, mjd, fiber-----#
 plate, mjd, fiber = query_plate_mjd_fiber(20, primtarget, zlim[0], zlim[1])
 print plate,mjd,fiber
 
+#-----plugging into fetch_sdss_spectrum() and plotting-----#
 for plate_n, mjd_n, fiber_n in zip(plate, mjd, fiber):
     print plate_n, mjd_n, fiber_n
-
     spec = fetch_sdss_spectrum(plate_n, mjd_n, fiber_n)    
     l = plt.plot(spec.wavelength(), spec.spectrum)
-    c = l[0].get_color()
+    c = l[0].get_color() #different color to each spectra
 
 plt.xlim(3000, 10000)
-
 plt.show()
